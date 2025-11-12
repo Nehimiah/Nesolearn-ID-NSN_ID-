2b64f2e43075774871928fd0abcaa5219165766c
Nehy <74353375+Nehimiah@users.noreply.github.com>
2025-11-12 15:32:03 -0800

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
       title="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Credentials to future-proof your career."
-      image="https://pub-b365ff8e20e448c3a7257b35173d9a38.r2.dev/Nesolearn%20ID%20images/login.png"
+      image="https://pub-b365ff8e20e448c3a7257b35173d9a38.r2.dev/Nesolearn%20ID%20images/login-removebg-preview.png"
       formType="login"
     />
   )
